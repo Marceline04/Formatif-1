341d68ddec61b40bf23ea049e0f8fcefbc56163e
MarcelineTambwe <marcelinetambwee@gmail.com>
2020-12-07 18:53:37 +0000

    added les devinette

--- sommatif1/app.py
+++ sommatif1/app.py
@@ -1,3 +1,7 @@
 #progamme interactif
 #Marceline Tambwe, tammar28@ecolecatholique.ca
 #2020-12-05
+
+
+print(Qu'est ce qui contient du sucre et qui malgré tout n'est pas sucré) le sucrier
+print(Il faut pédaler pour me faire avancer. On peut monter sur ma selle. J'ai un gidon et deux roues) vélo
