85bff6b570cc67495441ba075a7f1f277b40fcc1
MarcelineTambwe <marcelinetambwee@gmail.com>
2020-12-07 20:32:45 +0000

    progres

--- sommatif1/app.py
+++ sommatif1/app.py
@@ -3,5 +3,24 @@
 #2020-12-05
 
 
-print(Qu'est ce qui contient du sucre et qui malgré tout n'est pas sucré) le sucrier
-print(Il faut pédaler pour me faire avancer. On peut monter sur ma selle. J'ai un gidon et deux roues) vélo
+print("Qu'est ce qui contient du sucre et qui malgré tout n'est pas sucré")
+#le sucrier
+print("Il faut pédaler pour me faire avancer. On peut monter sur ma selle. J'ai un gidon et deux roues") 
+#vélo
+print("pourquoi le poulet at-il traversé la route")
+#pour aller de l'autre Cote 
+print("Combien de pays y a t'il en Afrique")
+#54
+#90
+#63
+#33
+print("quel langue est la plus parler au monnde")
+print("a. Anglais")
+#Mandarin 
+#Francais 
+#L'espagnol
+choix = input().lower()
+if choix == "a":
+  print("non, ce n'est pas l'anglais")
+elif choix == "b":
+  print("oui!!!! bravo")
