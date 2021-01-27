e13c0e0de7aff59c2db3ecf61e553f531be8da5f
MarcelineTambwe <marcelinetambwee@gmail.com>
2021-01-27 17:26:10 +0000

    almost there

--- sommatif/projet.py
+++ sommatif/projet.py
@@ -1,9 +1,19 @@
 import pgzrun
-pgzrun.g3o()
+
+images = [ ]
+img1= Actor("Driving",pos= (x,y))
+images.append(img1)
+img2= Actor (shopping)
+images.append(Actor("Sleeping.JPG", pos = (x, y))
+images.append(Actor("Shopping.jpeg", pos = (x, y))
+images.append(Actor("End.JPG", pos = (x, y))
+
+img1.dx = 2
+img2.dy = -1
 
 WIDTH = 600
 HEIGHT = 300
-Driving = Actor ("Driving.JPG")
+Driving = Actor("Driving.JPG")
 Driving.pos = (WIDTH, HEIGHT / 2)
 speed = 2
 
@@ -11,7 +21,8 @@ speed = 2
 def draw():
     screen.fill("yellow")
     Driving.draw()
-
+    Large_box = Rect((100, 100), (400, 300))
+   
 
 def on_mouse_down(pos):
     print("position: " + str(pos))
@@ -21,14 +32,5 @@ def on_mouse_down(pos):
 
 def update():
    Driving.x = Driving.x - speed
-
-
-images = [ ]
-img1= 'Actor'("Driving",pos= (x,y))
-images.append(img1)
-img2= 'Actor' (shopping)
-images.append(Actor("image2", pos = (x, y))
-images.append(Actor("End.JPG", pos = (x, y))
-
 
 pgzrun.g3o()
