a15d99ade7aec656f3a5a8530b64ae5687d380de
MarcelineTambwe <marcelinetambwee@gmail.com>
2021-01-28 20:30:26 +0000

    fini

--- sommatif/jeux.py
+++ sommatif/jeux.py
@@ -1,17 +1,32 @@
+# <en-tête>
+#Marceline Tambwe, tammar28@ecolecatholique.ca
+#2020-01-28
+
+#ajout les fonctions addictionel
+import pgzrun
 import random
 
+# set the size of the screen
 WIDTH, HEIGHT = 800, 600
+
+#Cree des objet 
 basketball = Actor("basketball")
 snake = Actor("snake")
+
+#La vitesse
+# (les valeurs de départ pour les variables du jeu)
 score=0
 basketball.speed = 1
 game_over = False
 
+
+#Définir une fonction qui...
 def reset_faster():
-    basketball.speed += 1
+    basketball.speed += 1 # = basketball.speed + 1
     basketball.x = 0
-    basketball.y = random.randint(0, HEIGHT - snake.height)
-    
+    basketball.y = random.randint(0, HEIGHT)
+
+# Définir une fonction qui est utiliser pour l'animation 
 def draw():
   screen.clear()
   screen.draw.text("Score: " + str(score), midtop = (WIDTH / 2, 5), fontsize = 40)
@@ -21,6 +36,7 @@ def draw():
   else:
     screen.draw.text("Game Over", center = (WIDTH / 2,HEIGHT / 2), fontsize = 40)
 
+# Définir une fonction qui est...  
 def update():
     global score, game_over
     if (basketball.x < WIDTH):
@@ -28,9 +44,13 @@ def update():
     else:
         game_over = True
         
-    if (alien.colliderect(basketball)):
+    if (snake.colliderect(basketball)):
         score += 1
         reset_faster()
 
+# Définir une fonction qui fait bouger l'objet
 def on_mouse_move(pos):
     snake.pos = pos
+
+#
+pgzrun.go()
